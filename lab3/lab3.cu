a8979d15d4caf8038a256781c44dc0a81e70bd17
scrashedward <scrashedward@gmail.com>
2017-05-21 02:48:44 +0800

    push code

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -51,7 +51,7 @@ __global__ void CalculateFixed(
 	int bidx = bgy * wb + bgx;
 
 	for(int i=0;i<3;i++){
-		if(mask[idx] == 0){
+		if(mask[idx] < 127){
 			fixed[idx*3 + i] = background[bidx*3 + i];
 			continue;
 		}
@@ -72,19 +72,19 @@ __global__ void CalculateFixed(
 
 		float r = 4 * tc - (tn + tw + ts + te);
 
-		if( x == 0 || mask[idx-1] == 0){
+		if( x == 0 || mask[idx-1] < 127){
 			if(bgx == 0) r += background[bidx * 3 + i];
 			else r += background[(bidx - 1) * 3 + i];
 		}
-		if( x == wt-1 || mask[idx+1] == 0){
+		if( x == wt-1 || mask[idx+1] < 127){
 			if(bgx == wb-1) r += background[bidx * 3 + i];
 			else r += background[(bidx + 1) * 3 + i];
 		}
-		if( y == 0 || mask[idx-wt] == 0){
+		if( y == 0 || mask[idx-wt] < 127){
 			if(bgy == 0 ) r += background[bidx * 3 + i];
 			else r += background[(bidx - wb) * 3 + i];
 		}
-		if( y == ht -1 || mask[idx+wt] == 0){
+		if( y == ht -1 || mask[idx+wt] < 127){
 			if(bgy == hb-1) r += background[bidx * 3 + i];
 			else r += background[(bidx + wb) * 3 + i];
 		}
@@ -96,37 +96,142 @@ __global__ void CalculateFixed(
 __global__ void PoissonImageCloningIteration(
 	const float *fixed,
 	const float *mask,
-	float *source, float *dest, int wt, int ht
+	float *source, float *dest, int wt, int ht, float w
 )
 {
 	int tx = threadIdx.x;
 	int ty = threadIdx.y;
 	int bx = blockIdx.x;
 	int by = blockIdx.y;
-	int x = bx * 32 + tx;
-	int y = by * 16 + ty;
+	int x = bx * blockDim.x + tx;
+	int y = by * blockDim.y + ty;
 	int idx = y * wt + x;
 
 	for(int i = 0; i < 3; i++){
-		if (mask[idx] == 0 ){
+		if (mask[idx] < 127 ){
 			dest[idx * 3 + i] = fixed[idx * 3 + i];
 			continue;
 		}
 		float r = fixed[idx * 3 + i];
-		if ( x != 0 && mask[idx-1] != 0 ){
+		if ( x != 0 && mask[idx-1] > 127 ){
 			r += source[(idx - 1)* 3+ i];
 		}
-		if ( x != wt - 1 && mask[idx + 1] != 0){
+		if ( x != wt - 1 && mask[idx + 1] > 127){
 			r += source[(idx + 1) * 3 + i];
 		}
-		if( y != 0 && mask[(idx - wt)] != 0){
+		if( y != 0 && mask[(idx - wt)] > 127){
 			r += source[(idx - wt) * 3 + i];
 		}
-		if( y != ht - 1 && mask[(idx + wt)] != 0){
+		if( y != ht - 1 && mask[(idx + wt)] > 127){
 			r += source[(idx + wt) * 3 + i];
 		}
-		dest[idx * 3 + i] = r/4;
+		dest[idx * 3 + i] = r/4 * w + (1-w) * source[idx * 3 + i];
 	}
+}
+
+__global__ void DownSample(
+	const float *input,
+	float *output,
+	int wt, int ht
+)
+{
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+	int bx = blockIdx.x;
+	int by = blockIdx.y;
+	int x = bx * 32 + tx * 2;
+	int y = by * 16 + ty * 2;
+
+	for(int i = 0; i < 3; i++){
+		float r = 0;
+		r += input[(y*wt + x) * 3 + i];
+		r += input[((y+1)*wt + x) * 3 + i];
+		r += input[(y * wt + x + 1) * 3 + i];
+		r += input[((y+1)*wt + x + 1) * 3 + i];
+		output[(y/2*wt/2 + x / 2)*3 + i] = r/4;
+	}
+
+}
+
+__global__ void UpSample(
+	const float *input,
+	float *output,
+	int wt, int ht
+)
+{
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+	int bx = blockIdx.x;
+	int by = blockIdx.y;
+
+	int x = bx * 16 + tx;
+	int y = by * 8 + ty;
+	int idx = y * wt + x;
+
+	float lu = 0, ld = 0, ru = 0, rd = 0;
+
+	for(int i = 0 ; i < 3 ; i++ ){
+		if(x == 0){
+			if(y == 0){
+				lu = input[idx * 3 + i];
+				ld = (input[idx * 3 + i] * 4 + input[(idx + wt) * 3 + i] * 2)/6;
+				ru = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2)/6;
+				rd = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt + 1) * 3 + i] * 1) / 9;
+			}
+			else if (y == ht-1) {
+				lu = (input[idx * 3 + i] * 4 + input[(idx - wt) * 3 + i] * 2)/6;
+				ld = input[idx * 3 + i];
+				rd = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2)/6;
+				ru = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt + 1) * 3 + i] * 1) / 9;
+			}
+			else{
+				lu = (input[idx * 3 + i] * 4 + input[(idx - wt) * 3 + i] * 2)/6;
+				ld = (input[idx * 3 + i] * 4 + input[(idx + wt) * 3 + i] * 2)/6;
+				ru = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt + 1) * 3 + i] * 1) / 9;
+				rd = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt + 1) * 3 + i] * 1) / 9;
+			}
+		}else if(x == wt -1){
+			if(y == 0){
+				ru = input[idx * 3 + i];
+				rd = (input[idx * 3 + i] * 4 + input[(idx + wt) * 3 + i] * 2)/6;
+				lu = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2)/6;
+				ld = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt - 1) * 3 + i] * 1) / 9;
+			}
+			else if(y == ht-1){
+				ru = (input[idx * 3 + i] * 4 + input[(idx - wt) * 3 + i] * 2)/6;
+				rd = input[idx * 3 + i];
+				ld = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2)/6;
+				lu = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt - 1) * 3 + i] * 1) / 9;
+			}
+			else{
+				ru = (input[idx * 3 + i] * 4 + input[(idx - wt) * 3 + i] * 2)/6;
+				rd = (input[idx * 3 + i] * 4 + input[(idx + wt) * 3 + i] * 2)/6;
+				lu = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt - 1) * 3 + i]) / 9;
+				ld = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt - 1) * 3 + i]) / 9;
+			}
+		}else if(y == 0){
+			ru = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2) / 6;
+			rd = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt + 1) * 3 + i]) / 9;
+			lu = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2) / 6;
+			ld = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt - 1) * 3 + i]) / 9;
+		}else if(y == ht-1){
+			rd = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2) / 6;
+			ru = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt + 1) * 3 + i]) / 9;
+			ld = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2) / 6;
+			lu = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt - 1) * 3 + i]) / 9;
+		}
+		else{
+			lu = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt - 1) * 3 + i]) / 9;
+			ld = (input[idx * 3 + i] * 4 + input[(idx - 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt - 1) * 3 + i]) / 9;
+			ru = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx - wt) * 3 + i] * 2 + input[(idx - wt + 1) * 3 + i]) / 9;
+			rd = (input[idx * 3 + i] * 4 + input[(idx + 1) * 3 + i] * 2 + input[(idx + wt) * 3 + i] * 2 + input[(idx + wt + 1) * 3 + i]) / 9;
+		}
+		output[(y * 2 * wt * 2 + x * 2) * 3 + i] = lu;
+		output[(y * 2 * wt * 2 + x * 2 + 1) * 3 + i] = ru;
+		output[(y * 2 * wt * 2 + wt * 2 + x * 2 + 1) * 3 + i] = rd;
+		output[(y * 2 * wt * 2 + wt * 2 + x * 2) * 3 + i] = ld;
+	}
+
 }
 
 void PoissonImageCloning(
@@ -138,32 +243,59 @@ void PoissonImageCloning(
 	const int oy, const int ox
 )
 {
-	float *fixed, *buf1, * buf2;
+	float *fixed, *buf1, * buf2, *df, *db1, *db2, *dmask;
 	cudaMalloc(&fixed, 3*wt*ht*sizeof(float));
 	cudaMalloc(&buf1, 3*wt*ht*sizeof(float));
 	cudaMalloc(&buf2, 3*wt*ht*sizeof(float));
+	cudaMalloc(&df, 3 * wt * ht * sizeof(float)/4);
+	cudaMalloc(&db1, 3 * wt * ht * sizeof(float) / 4);
+	cudaMalloc(&db2, 3 * wt * ht * sizeof(float) / 4);
+	cudaMalloc(&dmask, 3 * wt * ht * sizeof(float) /4);
 
 	dim3 gdim(CeilDiv(wt, 32), CeilDiv(ht, 16)), bdim(32,16);
 	CalculateFixed<<<gdim, bdim>>>(
 		background, target, mask, fixed,
 		wb, hb, wt, ht, oy, ox
 	);
+	
+	/** scaling part, please enable this part for scaling acceleration
+	DownSample<<<gdim, dim3(16, 8)>>>(fixed, df, wt, ht);
+	DownSample<<<gdim, dim3(16, 8)>>>(target, db1, wt, ht);
+	DownSample<<<gdim, dim3(16, 8)>>>(mask, dmask, wt, ht);
+	for(int i = 0 ; i < 5000; i++){
+		PoissonImageCloningIteration<<<gdim, dim3(16, 8)>>>(
+			df, dmask, db1, db2, wt/2, ht/2, 1
+		);
+		PoissonImageCloningIteration<<<gdim, dim3(16, 8)>>>(
+			df, dmask, db2, db1, wt/2, ht/2, 1
+		);
+	}
+	UpSample<<<gdim, dim3(16, 8)>>>(db1, buf1, wt/2, ht/2);
+	**/
+	
+	//Background acceleration part, please enable this part for background acceleration
+	//for(int i = 0; i < ht; i++){
+	// 	cudaMemcpy(buf1+i*wt*3, background + (oy*wb+ox) * 3 + i * wb * 3 , sizeof(float)*3*wt, cudaMemcpyDeviceToDevice);
+	//}
+
+	// Normal Copy, please disable this part for background acceleration and scaling acceleration
 	cudaMemcpy(buf1, target, sizeof(float)*3*wt*ht, cudaMemcpyDeviceToDevice);
 
 	cudaError_t error = cudaDeviceSynchronize();
 	if( error != cudaSuccess) cout << cudaGetErrorString(error) << endl;
-
-	for (int i = 0 ; i < 20000; i++){
+	
+	for (int i = 0 ; i < 10000 ; i++){
+		float w1 = 1, w2 = 1;
 		PoissonImageCloningIteration<<<gdim, bdim>>>(
-			fixed, mask, buf1, buf2, wt, ht
+			fixed, mask, buf1, buf2, wt, ht, w1
 		);
 		PoissonImageCloningIteration<<<gdim, bdim>>>(
-			fixed, mask, buf2, buf1, wt, ht
+			fixed, mask, buf2, buf1, wt, ht, w2
 		);
 	}
 	error = cudaDeviceSynchronize();
 	if( error != cudaSuccess) cout << cudaGetErrorString(error) << endl;
-
+	
 	cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
 	SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
 		background, buf1, mask, output,
